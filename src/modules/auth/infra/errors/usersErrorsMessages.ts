ef58e6f044c4fb478a5ccdb00c27c550f326243c
luiscaputo <luiscaputo15@gmail.com>
2022-12-01 06:57:19 +0100

    feat: Users errors messages

---
+++ src/modules/auth/infra/errors/usersErrorsMessages.ts
@@ -0,0 +1,7 @@
+export enum usersErrorsMessages {
+  userAlreadyExists = 'Esse usuário Já Existe',
+  userNotFound = 'Usuário não encontrado',
+  incorrectEmailOrPassword = 'Email/Palavra Passe errado',
+  passwordIsRequired = 'Palavra Passe é Obrigatória',
+  emailIsRequired = 'Email é obrigatório',
+}
