24a0049e1996819381188beaf78092cfb0e4a4e8
luiscaputo <luiscaputo15@gmail.com>
2022-12-01 07:04:14 +0100

    feat: Tasks errors messages

---
+++ src/modules/tasks/infra/errors/tasksErrosMessages.ts
@@ -0,0 +1,6 @@
+export enum tasksErrorsMessages {
+  invalidDateToStartTack = 'Data Inválida para começar a tarefa',
+  invalidDateQuantification = 'Quantificação de Data Inválida',
+  memberNotFound = 'Membro Não encontrado',
+  taskNotFound = 'Tarefa não encontrado',
+}
