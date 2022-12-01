94fa7974852cac6e2b6db0b35997eda4d27f3c23
luiscaputo <luiscaputo15@gmail.com>
2022-12-01 07:00:46 +0100

    feat: Users routes

---
+++ src/modules/auth/infra/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { UpdateUserController } from '../../useCases/updateUser/UpdateUserController';
+import { updateUser } from '../validators/users.validators';
+
+const routes = Router();
+
+routes.put('/users/update', updateUser, new UpdateUserController().handle);
+
+export default routes;
